8e5c93c55223c206b9ba88dcd9083606c1e6a2ed
sardylan <sardylan@gmail.com>
2018-12-19 14:39:28 +0100

    Adds FastCGI server

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
+from flup.server.fcgi import WSGIServer
+
 from app import app
+
+if __name__ == "__main__":
+    # app.run(
+    #    host="0.0.0.0",
+    #    port=7980,
+    #    threaded=True,
+    #    debug=True
+    # )
+
+    WSGIServer(app, bindAddress=("127.0.0.1", 7980)).run()
